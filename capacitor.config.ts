ad6fae62589fdbf273b2bf95e5f2306fecaa12f1
Nancy <nancy9@sheridancollege.ca>
2024-06-24 21:39:00 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'covidStats',
+  webDir: 'www'
+};
+
+export default config;
